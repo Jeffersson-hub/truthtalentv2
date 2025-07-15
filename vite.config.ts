b210ac325916cd70d65cf1c019e2dc6f1bec2e30
scorjeff <jf.boisgontier@gmail.com>
2025-07-15 10:52:56 +0200

    aajout node et upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path'; // ⬅️ nécessaire pour gérer les chemins
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'), // ⬅️ ajoute l'alias @ vers /src
+      '@': path.resolve(__dirname, 'src'),
     },
-  },
-  optimizeDeps: {
-    exclude: ['lucide-react'],
   },
 });
